1ac58b7d2eb8caa15de75a57acb7a699bbdf9a4b
Ned Twigg <ned.twigg@diffplug.com>
2024-07-05 20:13:20 -0700

    Move `ProcessRunner` and such out of `lib` and into `plugin-gradle`.

--- spotless-changelog-lib/src/main/java/com/diffplug/spotless/changelog/RingBufferByteArrayOutputStream.java
+++ spotless-changelog-lib/src/main/java/com/diffplug/spotless/changelog/RingBufferByteArrayOutputStream.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.diffplug.spotless.changelog.gradle;
+package com.diffplug.spotless.changelog;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
